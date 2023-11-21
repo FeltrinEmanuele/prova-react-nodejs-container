780ca7e625a8e52cea05e7b911b0fe5669c0d9a9
FeltrinEmanuele <Emanuelefeltrin19971010@gmail.com>
2023-11-21 15:32:39 +0100

    Aggiunta prova

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>{`Hello ${process.env.REACT_APP_NAME}`}</p>
+        <p>{`Hello ${process.env.REACT_APP_PROVA}`}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
